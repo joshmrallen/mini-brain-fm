61fc0d063b5c8df184e2fbd72924a4c9336b036f
Josh Allen <40782086+joshmrallen@users.noreply.github.com>
2022-05-27 16:00:49 -0400

    added preliminary css to index css; added MentalStateCard component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends React.Component {
     render(){
 
         return(
-            <div><h1>Active with MiniBrain.fm</h1></div>
+            <div id="title"><h1>Activate with MiniBrain.fm</h1></div>
         );
     }
 }
