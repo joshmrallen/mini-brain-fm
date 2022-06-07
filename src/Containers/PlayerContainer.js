53f5ccc0756a3a3e88520740b59326aa8f19f747
Josh Allen <40782086+joshmrallen@users.noreply.github.com>
2022-06-07 15:50:02 -0400

    add icon library; added buttons; added onClicks; TODO: add skip icons/buttons

--- src/Containers/PlayerContainer.js
+++ src/Containers/PlayerContainer.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {AiFillCloseCircle} from 'react-icons/ai'
+import {AiOutlineArrowLeft} from 'react-icons/ai'
+import {AiOutlineArrowRight} from 'react-icons/ai'
+import {FaPlay} from 'react-icons/fa'
+import {FaPause} from 'react-icons/fa'
 
 export default function PlayerContainer({toggleMentalState}){
 
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const togglePlayPause = () => {
+        setIsPlaying(!isPlaying);
+        console.log("play/pause toggled!")
+    }
 
     return(
         <div id="player-container">
-            
+
+            <button onClick={toggleMentalState}><AiFillCloseCircle /></button>
+            <button><AiOutlineArrowLeft /></button>
+            <button onClick={togglePlayPause}>
+                {isPlaying ? <FaPause /> : <FaPlay />}
+            </button>
+            <button><AiOutlineArrowRight /></button>
+
         </div>
     );
 }
